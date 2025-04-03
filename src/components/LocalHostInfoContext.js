9deee90e626fbf5a8b9ce4b46491725c90869887
twd1509 <twd1509@naver.com>
2025-04-03 15:57:04 +0900

    upload LocalHostInfoContext.js

---
+++ src/components/LocalHostInfoContext.js
@@ -0,0 +1,15 @@
+/*
+let localhostIP = "localhost";
+export const LocalHostInfoContext = {
+    common: `http://${localhostIP}:9191`,
+    aianalysis: `http://${localhostIP}:3002`,
+    aiRequest: `http://${localhostIP}:9193`
+}
+*/
+
+let localhostIP = "52.79.76.146";
+export const LocalHostInfoContext = {
+common: `http://${localhostIP}:30002`,
+aianalysis: `http://${localhostIP}:30004`,
+aiRequest: `http://${localhostIP}:30005`
+}
